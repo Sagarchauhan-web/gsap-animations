09fe4874a73525f39f5d585d1442504b68b0a3ac
sagarchauhan-web <sagarchawan4646@gmail.com>
2026-03-04 20:26:05 +0530

    to, from, fromto, set

---
+++ pages/12a_learn-fromto/script.js
@@ -0,0 +1,35 @@
+import gsap from 'gsap';
+
+const bars = document.querySelectorAll('.bar');
+
+bars.forEach((bar, index) => {
+  gsap.fromTo(
+    bar,
+    {
+      scaleY: 0.4,
+    },
+    {
+      scaleY: 1.6,
+      duration: 0.6,
+      ease: 'sine.inOut',
+      repeat: -1,
+      yoyo: true,
+      delay: index * 0.1,
+    },
+  );
+});
+
+// gsap.fromTo(
+//   '.bar',
+//   {
+//     scaleY: 0.1,
+//     repeat: -1,
+//     yoyo: true,
+//     stagger: 0.2,
+//   },
+//   {
+//     scaleY: 1,
+//     repeat: -1,
+//     stagger: 0.2,
+//   },
+// );
